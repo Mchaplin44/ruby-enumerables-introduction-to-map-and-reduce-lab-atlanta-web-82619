58043ec5a41f407f9593ebd8267da06abaaf23c1
Martin Chaplin <martin.chaplin44@gmail.com>
2019-08-20 18:44:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def map_to_square(source_array)
   source_array.map{|a|a**2}
 end
 
-reduce_to_total(source_array,starting_point)
+def reduce_to_total(source_array,starting_point)
  array.reduce(0) { |sum, num| sum + num }
  puts sum
  end
